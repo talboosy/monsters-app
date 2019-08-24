f3be7bc6d923023f9ab06dd2eed0a332596ede6a
DESKTOP-O1J837U\talbo <talboosy@gmail.com>
2019-08-24 15:41:56 +0300

    Updates

---
+++ precache-manifest.a25df6a21f817283330ceccaf28986d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9000198d1f206c4ba5609eab2a8766ed",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "9db8c2606a081146f917",
+    "url": "/monsters-app/static/css/main.ce2914c2.chunk.css"
+  },
+  {
+    "revision": "c429a589698ec663bba7",
+    "url": "/monsters-app/static/js/2.c4a20a25.chunk.js"
+  },
+  {
+    "revision": "9db8c2606a081146f917",
+    "url": "/monsters-app/static/js/main.2a278738.chunk.js"
+  },
+  {
+    "revision": "70f2ec99d9194a750eef",
+    "url": "/monsters-app/static/js/runtime~main.b6edfce3.js"
+  }
+]);
